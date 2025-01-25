10192fd903d0e9b13dcafb5f47584bdf654eff4d
Rohit Kushwaha <rajdilkhush0987654321@gmail.com>
2025-01-25 17:34:07 +0530

    add: Tsc-alias and tsconfig-paths

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -15,6 +15,17 @@ export default tseslint.config({
     rules: {
         "no-console": "error",
         quotes: ["error", "double", { allowTemplateLiterals: true }]
+    },
+    plugins: {
+        "@typescript-eslint": tseslint.plugin,
+        "@typescript-parser": tseslint.parser
+    },
+    settings: {
+        "import/resolver": {
+            typescript: {
+                project: "./tsconfig.json"
+            }
+        }
     }
 })
 
